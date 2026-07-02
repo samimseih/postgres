e2484f9ee394e431c110e5aec8a860c58468a4b4
Sami Imseih <samimseih@gmail.com>
2026-07-01 20:30:05 -0500

    test: demonstrate DSA allocation failure with shared_size > MaxAllocSize for variable kind
    Setting shared_size to 1GB (exceeding MaxAllocSize) causes
    dsa_allocate_extended() to ERROR with "invalid DSA memory alloc request
    size" when creating any entry. The server starts but the stats kind is
    completely unusable. This needs a guard in pgstat_register_kind().

--- src/test/modules/test_custom_stats/test_custom_fixed_stats.c
+++ src/test/modules/test_custom_stats/test_custom_fixed_stats.c
@@ -54,7 +54,7 @@ static const PgStat_KindInfo custom_stats = {
 	.shared_data_off = offsetof(PgStatShared_CustomFixedEntry, stats),
 	.shared_data_len = sizeof(((PgStatShared_CustomFixedEntry *) 0)->stats),
 
-	.init_shmem_cb = NULL,
+	.init_shmem_cb = test_custom_stats_fixed_init_shmem_cb,
 	.reset_all_cb = test_custom_stats_fixed_reset_all_cb,
 	.snapshot_cb = test_custom_stats_fixed_snapshot_cb,
 };
